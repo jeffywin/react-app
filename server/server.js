41224de4d9e003621271513d1db7efda272c175f
jeffywin <jeffywin@126.com>
2018-01-09 20:54:54 +0800

    react基础

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-// 链接mongoDb，并且使用react-app这个集合
+// 链接mongoDb，并且使用react-app这个
 const DB_URL = 'mongodb://127.0.0.1:27017/react-app'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', function() {
--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
+class App extends React.Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const boss = 'suning'
+    return(
+      <div>
+        <h1>公司是{boss}</h1>
+        <Project name='学习React,Vue'></Project>
+        <Learn name='node.js'></Learn>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+function Learn(props) {
+  return <h2>还要学习{props.name}</h2>
+}
+
+class Project extends React.Component {
+  constructor(props) { // 初始化数据
+    super(props)
+    this.state = {
+      book: ['vue', 'nodejs', 'react']
+    }
+    // this.readBook = this.readBook.bind(this) 强制把this绑定在函数上  或通过箭头函数 包括render内和写方法时候
+  }
+  readBook = () => {
+    this.setState({
+      book:[...this.state.book, 'jquery'+ Math.random()]
+    })
+  }
+  render() {
+    // const name = '学习React'
+    return(
+      <div>
+        <h2>我正在{this.props.name}</h2>
+        {/*<button onClick={() => this.readBook()}>点击阅读</button>*/}
+        <button onClick={this.readBook}>点击阅读</button>
+        <ul>
+          {this.state.book.map( v => {
+            return <li key={v}>{v}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
