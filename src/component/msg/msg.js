ea056f42caf7873ecd71ca3c75cb66e630f4a79f
jeffywin <jeffywin@126.com>
2018-03-15 08:28:16 +0800

    chatGrout

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {Button} from 'antd-mobile'
+import {connect} from 'react-redux'
 
+@connect ( 
+	state => state
+)
 class Msg extends React.Component {
 	constructor(props){
 		super(props);
 	}
 	render() {
+		const chatGroup = []
+		this.props.chat.chatmsg.forEach((v)=>{
+			chatGroup[v.chatid] = chatGroup[v.chatid] || []
+		})
 		return (
 			<div>
 				消息列表
